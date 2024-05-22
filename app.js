a6ed4912f8039a07ee917cb20ebf4fba04c2b949
Mohammad Atiq <154668693+MohammadAtiq11@users.noreply.github.com>
2024-05-22 22:44:36 +0500

    Chapter 05 - MATH EXPRESSIONS

---
+++ app.js
@@ -0,0 +1,212 @@
+// Q:1
+// A:1
+// let _1stValue = 3;
+// let _2ndValue = 8;
+// let sum = _1stValue + _2ndValue;
+// document.write(
+//   " Sum of ",
+//   _1stValue,
+//   " and ",
+//   _2ndValue,
+//   " is ",
+//   sum,
+//   "<br/><br/>"
+// );
+
+// Q:2
+// // A:2
+// let _3rdValue = 3;
+// let _4thValue = 8;
+// let sub = _3rdValue - _4thValue;
+// document.write(
+//   " Subtraction of ",
+//   _3rdValue,
+//   " and ",
+//   _4thValue,
+//   " is ",
+//   sub,
+//   "<br/><br/>"
+// );
+
+// let _5thValue = 3;
+// let _6thValue = 8;
+// let mul = _5thValue * _6thValue;
+// document.write(
+//   " multiplication of ",
+//   _5thValue,
+//   " and ",
+//   _6thValue,
+//   " is ",
+//   mul,
+//   "<br/><br/>"
+// );
+
+// let _7thValue = 3;
+// let _8thValue = 8;
+// let div = _7thValue / _8thValue;
+// document.write(
+//   " division of ",
+//   _7thValue,
+//   " and ",
+//   _8thValue,
+//   " is ",
+//   div,
+//   "<br/><br/>"
+// );
+
+// let _9thValue = 3;
+// let _10thValue = 8;
+// let reminder = _9thValue % _10thValue;
+// document.write(
+//   " reminder of ",
+//   _9thValue,
+//   " and ",
+//   _10thValue,
+//   " is ",
+//   reminder,
+//   "<br/><br/>"
+// );
+
+// Q:3
+// A:3
+// let a =
+//   document.write("Value after variable declaration is undefined", "</br></br>")
+// let c = 5;
+// document.write(" Initial value: ", c, "</br></br>")
+// let e = ++c;
+// document.write(" Value after increament is: ", e, "</br></br>")
+// let g = 7;
+// sum = g + e;
+// document.write(" Value after addition is: ", sum, "</br></br>")
+// let i = --sum
+// document.write(" Value after decreament is: ", i, "</br></br>")
+// let k = 3
+// reminder = i % k
+// document.write(" The reminder is: ", reminder, "</br></br>")
+
+// Q:4
+// A:4
+// let ticket = 600
+// let buying = 5
+// total = ticket * buying
+// document.write(" Total cost to buy ", buying, " tickets to movie is ", total, "PKR")
+
+// Q:5
+// A:5
+// let table = +prompt("Please Enter a number = ");
+// document.write("<h2/>", table, " x ", " 1 ", " = ", table * 1, "</br>");
+// document.write("<h2/>", table, " x ", " 2 ", " = ", table * 2, "</br>");
+// document.write("<h2/>", table, " x ", " 3 ", " = ", table * 3, "</br>");
+// document.write("<h2/>", table, " x ", " 4 ", " = ", table * 4, "</br>");
+// document.write("<h2/>", table, " x ", " 5 ", " = ", table * 5, "</br>");
+// document.write("<h2/>", table, " x ", " 6 ", " = ", table * 6, "</br>");
+// document.write("<h2/>", table, " x ", " 7 ", " = ", table * 7, "</br>");
+// document.write("<h2/>", table, " x ", " 8 ", " = ", table * 8, "</br>");
+// document.write("<h2/>", table, " x ", " 9 ", " = ", table * 9, "</br>");
+// document.write("<h2/>", table, " x ", " 10 ", " = ", table * 10, "</br>");
+
+// Q:6
+// A:6
+// let Celsius = 25
+// let multiply = 9
+// let divide = 5
+// let sum = 32
+// total = Celsius * multiply / divide + sum
+// document.write("<h2/> a.)", " 25°C is ", total, "°F")
+
+// let Fahrenheit = 70
+// let sub = 32
+// let mult = 5
+// let div = 9
+// total = (Fahrenheit - sub) * mult / div
+// document.write("<h2/> c.)", " 70°F is ", total, "°C")
+
+// Q:7
+// A:7
+// let item1 = +prompt("Price of item 1")
+// let qty1 = +prompt("Ordered quantity of item 1")
+// let item2 = +prompt("Price of item 2")
+// let qty2 = +prompt("Ordered quantity of item 2")
+// let shipping = +prompt("Shipping charges")
+
+// document.write("<h1/> Shopping Cart:", "<br><br>")
+// document.write("<h3/> Price of item ", 1, " is ", item1)
+// document.write("<h3/> Quantity of item ", 1, " is ", qty1)
+// document.write("<h3/> Price of item ", 2, " is ", item2)
+// document.write("<h3/> Quantity of item ", 2, " is ", qty2)
+// document.write("<h3/> Shipping Charges: ", shipping, "<br><br>")
+
+// let total1 = (item1 * qty1)
+// let total2 = (item2 * qty2)
+// let charges = (shipping)
+// let grand_total = total1 + total2 + charges
+
+// document.write("<h2/> Total cost of your order is ", grand_total)
+
+// Q:8
+// A:8
+// let t_marks = +prompt("Enter your total marks")
+// let m_obtained = +prompt("Enter your obtained marks")
+// let per = m_obtained / t_marks * 100
+
+// document.write("<h1/> Marks Sheet:", "<br><br>")
+
+// document.write("<h3> Total marks: ", "<strong>", t_marks)
+// document.write("<h3> Marks obtained: ", "<strong>", m_obtained)
+// document.write("<h2> Percentage: ", "<strong>", per)
+
+// Q:9
+// A:9
+// let $1USD = +prompt("Exchange rates \nUSD to PKR")
+// let CurrentUSD = 278.25
+// document.write("<h1/>Currency in PKR:", "<strong>", "<br><br>")
+
+// document.write("<h3/>Total Currency in PKR: ", "<strong>", $1USD * CurrentUSD)
+
+// Q:10
+// A:10
+// let seq = 11 + 5 * 10 / 2
+// console.log(seq)
+
+// Q:11
+// A:11
+// let c_year = +prompt("Enter your Current year")
+// let b_year = +prompt("Enter your Birth year")
+// let age = c_year - b_year
+
+// document.write("<h1/>Age Calculator:", "<strong>", "<br><br>")
+// document.write("<h4/>Current Year: ", "<strong>", c_year)
+// document.write("<h4/>Birth Year: ", "<strong>", b_year)
+// document.write("<h4/>Your Age is: ", "<strong>", age)
+
+// Q:12
+// A:12
+// Skipped
+
+// Q:13
+// A:13
+// let fav_snack = prompt("Store your favorite snack")
+// let c_age = +prompt("Store your current age")
+// let est_max_age = +prompt("Store a maximum age")
+// let per_day_snack = +prompt(" Store an estimated amount per day")
+// let days = 365
+// let final = est_max_age - c_age * days * per_day_snack
+// console.log(final)
+
+let fav_snack = "lays"
+let c_age = 15
+let est_max_age = 65
+let per_day_snack = 3
+let days = 365
+let final = est_max_age - c_age * days * per_day_snack
+console.log(final)
+
+// document.write("Favourite Snack: ", fav_snack)
+// document.write("Current age: ", c_age)
+// document.write("Estimated Maximum Age: ", est_max_age)
+// document.write("Amount of snacks per day: ", per_day_snack)
+// document.write("You will need", per_day_snack * days, fav_snack, "to last you until the ripe old age of", est_max_age)
+
+
+
+
